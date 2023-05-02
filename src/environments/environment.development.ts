e700edaea7a8b0c6d44a80f5b8e00961ec435c51
kurtreyn <kurt.reynolds@twinspires.com>
2023-05-02 10:54:31 -0400

    initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'student-management-syste-a51c5',
+    appId: '1:525784262142:web:65df0c9a745a7fc8ad0caf',
+    storageBucket: 'student-management-syste-a51c5.appspot.com',
+    apiKey: 'AIzaSyC_3Rd3mkPOAWa4i0gWEqWw9WcYGmfaY48',
+    authDomain: 'student-management-syste-a51c5.firebaseapp.com',
+    messagingSenderId: '525784262142',
+  },};
